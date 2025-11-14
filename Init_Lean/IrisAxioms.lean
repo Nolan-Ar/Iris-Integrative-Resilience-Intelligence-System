53a4778d0a13db088c80421acdbb50607991c8b7
Nolan-Ar <arnaultnolan.pro@gmail.com>
2025-11-14 21:47:57 +0100

    Update IrisAxioms.lean

--- Init_Lean/IrisAxioms.lean
+++ Init_Lean/IrisAxioms.lean
@@ -2,5 +2,4 @@
 -- Import modules here that should be built as part of the library.
 import «IrisAxioms».iris_axioms
 import «IrisAxioms».validation_correctifs
-import «IrisAxioms».echange_energie
 import «IrisAxioms».iris_brique
