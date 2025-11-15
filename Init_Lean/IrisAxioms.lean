92dbd5999ed84f6b0c47b23d434c1303ce5298a8
Nolan <arnaultnolan@gmail.com>
2025-11-15 02:44:34 +0100

    correct main lean

--- Init_Lean/IrisAxioms.lean
+++ Init_Lean/IrisAxioms.lean
@@ -3,3 +3,7 @@
 import «IrisAxioms».iris_axioms
 import «IrisAxioms».validation_correctifs
 import «IrisAxioms».iris_brique
+import «IrisAxioms».iris_axioms_extended
+import «IrisAxioms».iris_exemples_numeriques
+import «IrisAxioms».iris_theoremes_extended
+import «IrisAxioms».iris_validation_complete
